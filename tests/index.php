525e537581a996c4224aed6a4f2e59b8497206b6
TheSavior <github@eli-white.com>
2013-03-19 02:24:16 -0700

    Updating json file to be it's own package

--- tests/index.php
+++ tests/index.php
@@ -1,7 +1,7 @@
 <?php
   require '../vendor/autoload.php';
 
-  $api = new \Fitbit\Api("d309d935df0b415e84721ef9c40c2379", "64cff6931e0547a8b403dad81c5ba664");
+  $api = new \Fitbit\Api("clientid", "clientsecret");
   //$api->resetSession();
   if (!$api->isAuthorized()) {
     $api->initSession();
@@ -12,5 +12,5 @@
 
 <pre>
 <?php /// var_dump($api->getTimeSeries("startTime", "2013-03-01", "3m")); ?>
-<?= var_dump($api->getRateLimit()); ?>
+<?= var_dump($api->getProfile()); ?>
 </pre>
